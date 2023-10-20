480a640286001771729dc8d7d5e03cfa86f654c7
ChaseLoeffler <chaselo609@gmail.com>
2023-10-20 18:39:56 -0400

    Wote code so MusicTable would return a table containing all the songs from the backend.

--- music-library-frontend/src/Components/MusicTable/MusicTable.jsx
+++ music-library-frontend/src/Components/MusicTable/MusicTable.jsx
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
-
+import moment from 'moment';
 
 const MusicTable = (props) => {
+
     return (
         <div>
             <table>
+                <thead className='table'>
                 <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>ReleaseDate</th>
+                    <th>Genre</th>
                 </tr>
+                </thead>
+                <tbody>
+                    {props.songs.map((song, index) => {
+                        return(
+                            <tr key={index}>
+                                <td>{song.title}</td>
+                                <td>{song.artist}</td>
+                                <td>{song.album}</td>
+                                <td>{moment(song.releaseDate).format('MM/DD/YYYY')}</td>
+                                <td>{song.genre}</td>
+                                <td><button>Delete Song</button></td>
+                                <td><button>Edit song</button></td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
             </table>
         </div>
     );
